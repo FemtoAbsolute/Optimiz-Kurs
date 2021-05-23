bc11b2fe1c5f0bad4ad0e17fc3f76d6366f957ea
Игорь Осипов <62898482+garinich777@users.noreply.github.com>
2021-05-23 23:56:00 +0300

    Добавлена возможность пересчитывать данные для графика

---
+++ Optimiz_Kurs/Calculation/Functions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimiz_Kurs.Calculation
+{
+    public static class Functions
+    {
+        public static double f0(double T1, double T2) => (1.0 * (2.0 * 1.0) * (Math.Pow(T1 - T2, 2) + Math.Pow(1.0 * 1.0 - T1, 2))) * 100; 
+    }
+}
